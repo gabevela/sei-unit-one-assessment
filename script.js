c7fb4e10be3e68b1dc0b7979603e05e02e188164
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-04-09 14:56:55 -0400

    unit 1 assesment

--- script.js
+++ script.js
@@ -2,4 +2,31 @@ let counter = 0;
 
 const inputElement = document.getElementById("input");
 const displayElement = document.getElementById("display");
+const addElement = document.getElementById("plus-button");
+const minusElement = document.getElementById("minus-button");
+
+addElement.addEventListener('click' , addition);
+minusElement.addEventListener('click' , subtraction);
+
+function addition() {
+    counter = counter + parseInt(inputElement.value);
+    displayElement.innerText = counter;
+    if (counter < 0) {
+        displayElement.style.color = "red";
+    } else {
+        displayElement.style.color = "black"
+    }
+}
+
+function subtraction() {
+    counter = counter - parseInt(inputElement.value);
+    displayElement.innerText = counter;
+    if (counter < 0) {
+        displayElement.style.color = "red";
+    } else {
+        displayElement.style.color = "black"
+    }
+}
+
+
 
